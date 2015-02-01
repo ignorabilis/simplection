3fe65d45e3501c4e7896271f071ad29fc42b30e8
Elian Kurtenkov <elian90@abv.bg>
2015-02-01 14:44:53 +0200

    WS support

--- project.clj
+++ project.clj
@@ -8,22 +8,26 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.5"]
                  [ring/ring-core "1.3.2"]
+                 [jumblerg/ring.middleware.cors "1.0.1"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljs-http "0.1.21"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [http-kit "2.0.0"]
+                 [cheshire "5.4.0"]]
 
   :profiles {:dev
              {:dependencies [[criterium "0.4.3"]
-                             [midje "1.6.3"]]}}
+                             [midje "1.6.3"]
+                             [javax.servlet/servlet-api "2.5"]]}}
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-ring "0.8.13"]
             [lein-pdo "0.1.1"]]
 
-  :ring {:handler simplection.handler/app}
+  :main simplection.core
 
-  :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
+  :aliases {"up" ["pdo" "run," "cljsbuild" "auto" "dev"]}
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljs/simplection"]
--- src/clj/simplection/core.clj
+++ src/clj/simplection/core.clj
@@ -1,1 +1,43 @@
-(ns simplection.core)
+(ns simplection.core
+  (:use [compojure.core :only (defroutes GET)]
+        ring.util.response
+        org.httpkit.server)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.middleware.cors :refer [wrap-cors]]
+            [cheshire.core :refer :all]))
+
+(def clients (atom {}))
+
+(defn ws
+  [req]
+  (with-channel req con
+                (swap! clients assoc con true)
+                (println con " connected")
+                (on-close con (fn [status]
+                                (swap! clients dissoc con)
+                                (println con " disconnected. status: " status)))))
+
+(defroutes routes
+  (GET  "/" [] (resource-response "index.html" {:root "public"}))
+  (GET "/ws" [] ws)
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def application (-> (handler/site routes)
+                     (wrap-cors
+                      :access-control-allow-origin #"http://simplection.com.*$")))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt
+               (or (System/getenv "PORT") "4321"))]
+    (run-server application {:port port :join? false})))
+
+
+(future (loop []
+          (doseq [client @clients]
+             (send! (key client) (generate-string
+                                 {:happiness (rand 10)})
+                   false))
+          (Thread/sleep 1000)
+          (recur)))
